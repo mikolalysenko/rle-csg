eb704a474ff80fe56e0dfe097919410990458407
Mikola Lysenko <mikolalysenko@gmail.com>
2013-02-05 09:50:13 -0800

    added other functions

--- csg.js
+++ csg.js
@@ -1,5 +1,6 @@
-var merge = require("rle-funcs").merge;
+var funcs = require("rle-funcs");
 
+//Subtract
 var SUBTRACT_FUNC = new Function("phases", "distances", "retval", [
   "if(phases[0]) {",
     "if(phases[1]) {",
@@ -19,18 +20,58 @@ var SUBTRACT_FUNC = new Function("phases", "distances", "retval", [
     "}",
   "}"
 ].join(""));
-
-var POINT_STENCIL = new Int32Array(3);
-
-//CSG
-exports.subtract   = function(a, b) { return merge([a,b], POINT_STENCIL, SUBTRACT_FUNC); }
-
-exports.complement = function(a) {
-  var result = a.clone();
-  for(var i=0; i<result.length(); ++i) {
-    result.phases[i] ^= 1;
-  }
-  return result;
+exports.subtract   = function(a, b) {
+  return funcs.mergePoint([a,b], SUBTRACT_FUNC);
 }
 
+//Unite
+var UNITE_FUNC = new Function("phases", "distances", "retval", [
+  "if(phases[0]) {",
+    "retval[0] = 1;",
+    "if(phases[1]) {",
+      "retval[1] = Math.max(distances[0], distances[1]);",
+    "} else {",
+      "retval[1] = distances[0];",
+    "}",
+  "} else {",
+    "if(phases[1]) {",
+      "retval[0] = 1;",
+      "retval[1] = distances[1];",
+    "} else {",
+      "retval[0] = 0;",
+      "retval[1] = Math.min(distances[0], distances[1]);",
+    "}",
+  "}"
+].join(""));
+exports.unite   = function(a, b) {
+  return funcs.mergePoint([a,b], UNITE_FUNC);
+}
 
+//Intersect
+var INTERSECT_FUNC = new Function("phases", "distances", "retval", [
+  "if(phases[0]) {",
+    "if(phases[1]) {",
+      "retval[0] = 1;",
+      "retval[1] = Math.min(distances[0], distances[1]);",
+    "} else {",
+      "retval[0] = 0;",
+      "retval[1] = distances[1];",
+    "}",
+  "} else {",
+    "retval[0] = 0;",
+    "if(phases[1]) {",
+      "retval[1] = distances[0];",
+    "} else {",
+      "retval[1] = Math.max(distances[0], distances[1]);",
+    "}",
+  "}"
+].join(""));
+exports.intersect  = function(a, b) {
+  return funcs.mergePoint([a,b], INTERSECT_FUNC);
+}
+
+//Complement
+var COMPLEMENT_FUNC = new Function("p", "d", "retval", "retval[0] = p^1; retval[1] = d;");
+exports.complement = function(a) {
+  return funcs.applyPoint(a, COMPLEMENT_FUNC);
+}
--- example/index.js
+++ example/index.js
@@ -8,26 +8,21 @@ $(document).ready(function() {
   var viewer = require("gl-shells").makeViewer();
  
   //Create a box
-  var box = core.sample([-10, -10, -10], [10, 10, 10], function(x) {
-    if(Math.max.apply(null, x) < 5) {
-      return 1;
-    }
-    return 0;
+  var box = core.sampleSolid([-10, -10, -10], [10, 10, 10], function(x) {
+    return Math.max(Math.abs(x[0]), Math.abs(x[1]), Math.abs(x[2])) - 7.0;
   });
   
   //Create a sphere
-  function sphere_dist(x) {
-    return Math.sqrt(x[0]*x[0]+x[1]*x[1]+x[2]*x[2]) - 5.5;
-  }
-  var sphere = core.sample([-6,-6,-6], [7,7,7], function(x) {
-    if(sphere_dist(x) < 0) {
-      return 1;
-    }
-    return 0;
-  }, sphere_dist);
+  var sphere = core.sampleSolid([-10,-10,-10], [10,10,10], function(x) {
+    return Math.sqrt(x[0]*x[0]+x[1]*x[1]+x[2]*x[2]) - 7.5;
+  });
+  
+  var bigsphere = core.sampleSolid([-20,-20,-20], [20,20,20], function(x) {
+    return Math.sqrt(x[0]*x[0]+x[1]*x[1]+x[2]*x[2]) - 10.0;
+  });
 
   //Subtract shapes
-  var shape = csg.subtract(box, sphere);
+  var shape = csg.intersect(box, bigsphere);
   
   //Draw initial mesh
   viewer.updateMesh(mesh(shape));
