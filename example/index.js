fcdaf6bbcfd07851bba2f579eb685c1860a1f487
Mikola Lysenko <mikolalysenko@gmail.com>
2013-02-05 16:53:22 -0800

    added bundle

--- example/index.js
+++ example/index.js
@@ -7,6 +7,11 @@ $(document).ready(function() {
   //Create viewer
   var viewer = require("gl-shells").makeViewer();
  
+  //Create a cylinder
+  var cylinder = core.sampleSolid([-20, -20, -20], [20, 20, 20], function(x) {
+      return Math.max(Math.abs(x[2]) - 15, Math.sqrt(x[0]*x[0] + x[1]*x[1]) - 5.5);
+  });
+ 
   //Create a box
   var box = core.sampleSolid([-10, -10, -10], [10, 10, 10], function(x) {
     return Math.max(Math.abs(x[0]), Math.abs(x[1]), Math.abs(x[2])) - 7.0;
@@ -14,15 +19,16 @@ $(document).ready(function() {
   
   //Create a sphere
   var sphere = core.sampleSolid([-10,-10,-10], [10,10,10], function(x) {
-    return Math.sqrt(x[0]*x[0]+x[1]*x[1]+x[2]*x[2]) - 7.5;
+    return Math.sqrt(x[0]*x[0]+x[1]*x[1]+x[2]*x[2]) - 8.0;
   });
   
+  //Create a bigger sphere
   var bigsphere = core.sampleSolid([-20,-20,-20], [20,20,20], function(x) {
     return Math.sqrt(x[0]*x[0]+x[1]*x[1]+x[2]*x[2]) - 10.0;
   });
 
   //Subtract shapes
-  var shape = csg.intersect(box, bigsphere);
+  var shape = csg.subtract(csg.unite(box, cylinder), sphere);
   
   //Draw initial mesh
   viewer.updateMesh(mesh(shape));
